8c2fd48a6204dbda6b41523689cb3f30195200fc
TANISHKNEGI <tanishknegi36@gmail.com>
2026-01-30 22:49:56 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def parse_time(ts: str) -> datetime:
-    return datetime.fromisoformat(ts)
-
-
-def is_overlap(a_start, a_end, b_start, b_end) -> bool:
-    return not (a_end <= b_start or b_end <= a_start)
